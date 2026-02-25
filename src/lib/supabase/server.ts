2993f697dc10bb624fc41445f5251f45ea36bb58
Adrien Haubrich <adrienhaubrich@MacBook-Pro-de-Adrien.local>
2026-02-25 13:46:47 +0100

    fix: correction page inscription et server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -39,9 +39,4 @@ export function createAdminClient() {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
   );
-},
-        setAll() {},
-      },
-    }
-  );
 }
